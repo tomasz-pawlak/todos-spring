34c5602f467052b11d7d00a536a49f8045aa5977
Tomasz Pawlak <s346072@labs.wmi.amu.edu.pl>
2020-08-10 15:05:06 +0200

    exepctionHandeler

--- src/main/java/com/gurtoc/todos/controller/TaskGroupController.java
+++ src/main/java/com/gurtoc/todos/controller/TaskGroupController.java
@@ -54,4 +54,15 @@ public class TaskGroupController {
         return ResponseEntity.noContent().build();
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity<?> handleIllegalArgument (IllegalArgumentException e){
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @ExceptionHandler(IllegalStateException.class)
+    ResponseEntity<?> handleIllegalState (IllegalStateException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
 }
